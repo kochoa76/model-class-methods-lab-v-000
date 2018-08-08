4a00dde5709aa7fa2668a596c34850fdae316a8a
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 20:41:35 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,9 +2,10 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def catamaran_operators
-    self.boats.map do |boat| 
+    self.boats.map do |boat|
       boat.classification.name == "catamaran"
   end
+end 
 
 
 end

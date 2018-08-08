31947323775fde739451fe73765de2d3847b3ae1
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 19:26:22 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,9 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    b = Boat.find_or_create(created_at: created_at)
+    return b.created_at[0,5]
+  end
 end

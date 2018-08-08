5cac8553e0959e3cea911f2607644f68a10e343c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 20:53:31 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,5 +5,7 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
-
+  def self.sailors 
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"})
+  end 
 end

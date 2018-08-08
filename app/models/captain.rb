4d5617d59bd20e19a563e519ece7cb3130893c1f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 20:55:45 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,6 +10,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    includes(boats: :classifications).where(classifications: {name: "Motorboats", "Sailboats"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat", "Sailboat"})
   end
 end
